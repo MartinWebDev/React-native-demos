f662681caf34f69830fefad8bde2bb36473a94e0
Martin <martin@martinwebdev.co.uk>
2017-04-20 17:02:02 +0800

    - Forgot to include new files in previous commit -

---
+++ Demos/FullNavigation.js
@@ -0,0 +1,51 @@
+/**
+ * To use react-navigation as show in this demo you need to add it to package.json and then run "npm install"
+ * At the time of writing this the current npm version was out of date, so a specific version from github needs adding:
+ *      - "react-navigation": "git+https://github.com/react-community/react-navigation.git#7165efc"
+ * Once at least beta.8 has been released to npm however you can simply reference the version number instead. 
+ *      - "react-navigation": "1.0.0-beta.8"
+ * Note: When running npm install, you may see some warnings and it may take a while, 
+ * this is fine as long as you don't have errors, just wait and let it finish.
+ */
+
+/**
+ * Since this demo uses quite a lot of components I have broken it into separate files to make it easier to read, manage, and edit
+ * You will find all the code in the folder ./RNav/ or check the import statements below. 
+ */
+
+// Import main React Components
+import React , { Component } from 'react';
+
+import {
+  Text, 
+  View, 
+  Button
+} from 'react-native';
+
+// Import components needed from react-navigation
+import { TabNavigator, StackNavigator } from 'react-navigation';
+
+// Import components needed for demo
+import { HomeScreen } from './RNav/HomeScreen';
+import { ProductsScreen } from './RNav/ProductsScreen';
+import { ShoppingCartScreen } from './RNav/ShoppingCartScreen';
+import { AccountDetailsScreen } from './RNav/AccountDetailsScreen';
+
+// Set up main demo screen and navigation
+const MainScreenNavigator = TabNavigator({
+    Recent: { screen: RecentChatsScreen }, 
+    All: { screen: AllContactsScreen }
+}, {
+    navigationOptions: {
+        headerVisible: false
+    }
+});
+
+MainScreenNavigator.navigationOptions = {
+  title: 'My Chats',
+};
+
+export const ReactNavDemo = StackNavigator({
+  Home: { screen: MainScreenNavigator },
+  Chat: { screen: ChatScreen },
+});
---
+++ Demos/RNav/ProductsScreen.js
@@ -0,0 +1,13 @@
+// Import main React Components
+import React , { Component } from 'react';
+
+import {
+  Text, 
+  View, 
+  Button
+} from 'react-native';
+
+// Build up and export component
+export class ProductsScreen extends Component {
+
+}
