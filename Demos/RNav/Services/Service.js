7247bd8956ac24208690e4787133d4d847e1cf15
Martin Wood <martin@martinwebdev.co.uk>
2017-05-10 10:04:23 +0800

    Api line referenced root when it shouldn't have

--- Demos/RNav/Services/Service.js
+++ Demos/RNav/Services/Service.js
@@ -2,6 +2,6 @@ export class Service {
     buildApiLocation(root, api) {
         return (root.charAt(root.length - 1) == "/" ? root.substr(0, root.length - 1) : root) + 
             "/" + 
-            (api.charAt(0) == "/" ? root.substr(1, api.length - 1) : api);
+            (api.charAt(0) == "/" ? api.substr(1, api.length - 1) : api);
     }
 }
